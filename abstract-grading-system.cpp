534eb72094954e18ed990b507a7f31e70b54d3aa
moeinpoi <moeinnaderi.iut@gmail.com>
2025-10-27 21:47:08 -0500

    Add abstract-grading-system solution

---
+++ abstract-grading-system.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+class Person {
+public: 
+    virtual ~Person() = default;    
+    virtual void printInfo() const = 0;
+};
+
+class Student : public Person {
+public:
+    Student() = default;
+    ~Student() override = default;
+    Student(const std::string& name): m_name(name){};
+    void printInfo() const override {
+        std::cout << getName() << " (" << getRole() << ") - Average: " << getAve() << std::endl; 
+    }  
+    virtual std::string getRole() const = 0;
+    virtual double getAve() const = 0;
+    virtual double getBaseAve() const {
+        double sum = 0;
+        for(double grade : m_grades) {
+            sum += grade;
+        }
+        return m_grades.empty() ? 0 : sum / m_grades.size();
+    }
+    const std::string& getName() const { return m_name;}
+
+public:
+    std::string m_name;
+    std::vector<double> m_grades;
+};
+
+class UnderGradStudent : public Student{
+public:
+    UnderGradStudent(const std::string& name) : Student(name) {};
+    double getAve() const override {
+        return getBaseAve();
+    }
+    std::string getRole() const override { return "Undergraduate";}
+};
+
+class GradStudent : public Student{
+public:
+    GradStudent(const std::string& name, double thesisScore) : Student(name) , m_thesisScore(thesisScore) {};
+    double getAve() const override {
+        double base = getBaseAve();
+        return (base*0.7) + (m_thesisScore*0.3);
+    }
+    std::string getRole() const override { return "Graduate";}
+
+public:
+    double m_thesisScore;
+};
+
+class ExchangeStudent : public Student{
+public:
+    ExchangeStudent(const std::string& name, int topScoreCount) : Student(name) , m_topScoreCount(topScoreCount) {};
+    double getAve() const override {
+        double sum = 0;
+        std::vector<double> grades = m_grades;
+        std::sort(grades.begin(), grades.end(), std::greater<double>());
+        for (int i = 0; i < m_topScoreCount; i++) {
+            sum += grades[i];
+        }
+        return  !m_topScoreCount ? 0 : sum / m_topScoreCount;
+    }
+    std::string getRole() const override { return "Exchange";}
+
+public:
+    int m_topScoreCount;
+};
+
+class Course{
+public:
+    Course() {m_students = {};}
+    Course(Student* student) {m_students.push_back(student);}
+    Course(std::vector<Student*> students) {
+        for (auto student : students) {
+            m_students.push_back(student);
+        }
+    }
+
+    void enroll_student(Student* student) {
+        m_students.push_back(student);
+    }
+    void enroll_student(std::vector<Student*> students) {
+        for (auto student : students) {
+            m_students.push_back(student);
+        }
+    }
+
+    void printRoster() const {
+        for (auto student : m_students) {
+            std::cout << "- " << student->getName() << ": Average= " << student->getAve() << std::endl;
+        }
+    }
+
+    double getCourseAve () const {
+        double courseTot = 0;
+        for (auto student : m_students) {
+            courseTot += student->getAve();
+        }
+        return m_students.empty() ? 0 : courseTot / m_students.size();
+    } 
+
+public:
+    std::vector<Student*> m_students;
+};
+
+
+int main () {
+
+    UnderGradStudent Moe("Moe");
+    Moe.m_grades.push_back(20);
+    Moe.m_grades.push_back(17);
+    Moe.m_grades.push_back(12);
+
+    GradStudent Joe("Joe", 20);
+    Joe.m_grades.push_back(12);
+    Joe.m_grades.push_back(14);
+
+    ExchangeStudent Foe("Foe", 2);
+    Foe.m_grades.push_back(20);
+    Foe.m_grades.push_back(19);
+    Foe.m_grades.push_back(7);
+    Foe.m_grades.push_back(12);
+
+    std::vector<Student*> additionalStudents = {&Foe};
+
+    Course FluidMech(&Moe);
+    FluidMech.enroll_student(&Joe);
+    FluidMech.enroll_student(additionalStudents);
+
+    for (auto student : FluidMech.m_students)
+        student->printInfo();
+}
