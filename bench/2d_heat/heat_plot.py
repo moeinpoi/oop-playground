4914bd346f4885dae39680080df68b57ff4d213b
moeinpoi <moeinnaderi.iut@gmail.com>
2026-07-09 21:58:49 -0500

    Modify plot code and output

--- bench/2d_heat/heat_plot.py
+++ bench/2d_heat/heat_plot.py
@@ -5,7 +5,11 @@ data = np.loadtxt("./2d_heat_output.csv", delimiter=',')
 
 print(data.shape)
 
-for t in [0, 19,99, 999]:
-    plt.plot(data[t,:])
+for step in [0, 20, 100, 1000]:
+    plt.plot(data[step - 1 if step else 0, :], label=f"timestep = {step}")
 
+plt.xlabel("grid index")
+plt.ylabel("temperature")
+plt.legend()
+plt.legend(loc="lower center")
 plt.show()
